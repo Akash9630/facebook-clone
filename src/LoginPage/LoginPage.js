7c1bcfc7afaec9828746500e3e33d3e4ddbd63eb
Akash_96 <realakash9630@gmail.com>
2022-10-04 18:32:45 +0530

    alter

---
+++ src/LoginPage/LoginPage.js
@@ -0,0 +1,14 @@
+import "./LoginPage.css"
+function LoginPage() {
+    return (
+        <div className="Login">
+            <div className="facebook">
+                <div className="facebooktext">
+                    facebook
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default LoginPage
